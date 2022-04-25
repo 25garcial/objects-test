20e05ff48340f4b7c2b94309058621feaf740346
25garcial <91680716+25garcial@users.noreply.github.com>
2022-04-25 11:51:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-function human(name, age, hairColor, race){
+let result=document.querySelector(".result")
+
+function human(name, age, hairColor, race, alive){
+let health=100;
+name=name.slice(0,1).toUpperCase()+name.slice(1,name.length).toLowerCase()
+
+if (alive=="alive" || alive==true){
+alive=true;
+}
+else if(alive=="dead" || alive == false){
+alive=false;
+}
+
+const resuscitate =(target) => {if (target.alive==false){
+health-=Math.floor(Math.random(1)*5);
+target.health=1;
+target.alive=true;
+result.textContent=`${name} resuscitated ${target.name} to 1 health and is now at ${health} health.`}else if(target.alive){result.textContent=(`${target.name} is already alive`)}
+
+}
+
 	const describe = () => {alert(`My name is ${name} and i have ${hairColor} hair and i am ${age} years old. I am ${race}.`)};
   
-  const fight= (enemy) => {let randomNumber = Math.floor(Math.random(1)*21); enemy.health-=randomNumber; console.log(`${this.name} has done ${randomNumber} damage to ${enemy.name}. ${enemy.name} has ${enemy.health} health left.`); if (enemy.health<1){console.log(`${enemy.name} is dead`); enemy.fight=''}}
+  const fight= (enemy) => {if (enemy.health<1) {let randomNumber = Math.floor(Math.random(1)*21); enemy.health-=randomNumber; if (enemy.health<0){enemy.health=0;} result.textContent=(`${name} has done ${randomNumber} damage to ${enemy.name}. ${enemy.name} has ${enemy.health} health left.`);} else {result.textConent=(`${enemy.name} is dead`); enemy.fight=''} enemy.alive=false}
   
-	return {name, age, hairColor, race, describe, fight}
+	return {health, name, age, hairColor, race, describe, fight, alive, resuscitate}
 }
 var bob=human("bob", 45, "gray", "latino")
-var john=human("john", 16, "blonde", "irish")
-john.fight(bob)
+var john=human("john", 16, "blonde", "japanese")
